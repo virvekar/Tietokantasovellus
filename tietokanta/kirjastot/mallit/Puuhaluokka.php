44b7262f57dbeb7c8a1d692f869c87e7a389e2a1
Virve E Maanpaa <virvemaa@svm-19.cs.helsinki.fi>
2014-04-02 15:49:41 +0300

    malleja muokattu

--- tietokanta/kirjastot/mallit/Puuhaluokka.php
+++ tietokanta/kirjastot/mallit/Puuhaluokka.php
@@ -33,5 +33,45 @@ class Puuhaluokka {
         return $this->kuvaus;
     }
     
+    public static function AnnaTiedotListaukseen(){
+        $sql = "SELECT puuhaluokanid, puuhaluokanNimi, puuhaluokanKuvaus FROM puuhaluokka";
+        $kysely = getTietokantayhteys()->prepare($sql);
+        $kysely->execute();
 
+        $tulokset = array();
+        foreach ($kysely->fetchAll(PDO::FETCH_OBJ) as $tulos) {
+            $puuhaluokka = new Puuhaluokka();
+            $puuhaluokka->setId($tulos->puuhaluokanid);
+            $puuhaluokka->setNimi($tulos->puuhaluokannimi);
+            $puuhaluokka->setKuvaus($tulos->puuhaluokankuvaus);
+
+
+            $tulokset[] = $puuhaluokka;
+        }
+        return $tulokset;
+    }
+    public static function MontakoPuuhaaLuokassa($luokanid){
+        $sql = "SELECT COUNT(puuhanid) AS luokanpuuhat FROM puuhat WHERE puuhaluokanid= ?";
+    $kysely = getTietokantayhteys()->prepare($sql);
+    $kysely->execute(array($luokanid));
+
+    $tulos = $kysely->fetchObject();
+    return $tulos->luokanpuuhat;
+    
+    }
+    
+    public static function AnnaViimeisinLisaysPaiva($luokanid){
+        $sql = "Select max(puuhanLisaysPaiva) AS viimeisinpaiva
+                from puuhat
+                where puuhaluokanid = ?";
+        $kysely = getTietokantayhteys()->prepare($sql);
+        $kysely->execute(array($luokanid));
+
+        $tulos = $kysely->fetchObject();
+        if(is_null($tulos->viimeisinpaiva)){
+            return "-";
+        }
+        return $tulos->viimeisinpaiva;
+
+    }
 }
