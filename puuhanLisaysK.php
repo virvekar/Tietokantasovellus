033f5c877a613865a3a5b6596c1faa842e1d88c7
Virve E Maanpaa <virvemaa@svm-19.cs.helsinki.fi>
2014-04-05 19:22:30 +0300

    Puuhan lisays

--- puuhanLisaysK.php
+++ puuhanLisaysK.php
@@ -2,14 +2,86 @@
 require_once 'tietokanta/kirjastot/nakymakutsut.php';
 require_once 'tietokanta/kirjastot/tietokantayhteys.php';
 require_once 'tietokanta/kirjastot/onkoKirjautunut.php';
+require_once 'tietokanta/kirjastot/mallit/Puuhaluokka.php';
+require_once 'tietokanta/kirjastot/mallit/Taidot.php';
+require_once 'tietokanta/kirjastot/mallit/Puuhat.php';
+require_once 'tietokanta/kirjastot/annaKirjautuneenNimimerkki.php';
 
 /* Tarkistetaan onko käyttäjä kirjautunut sisään*/
 if (!OnkoKirjautunut()) {
-    naytaNakyma('tietokanta/nakymat/Kirjautuminen.php', array(        
+    naytaNakyma('nakymat/Kirjautuminen.php', array(        
         'virhe' => "Kirjaudu sisään lisätäksesi puuhan.", request
     ));
 }
-naytaNakyma('tietokanta/nakymat/puuhanLisays.php', array(
-    'aktiivinen' => "puuhat"
+   
+$luokat = Puuhaluokka::AnnaTiedotListaukseen();
+$taidot = Taidot::AnnaTaitoListaus();
+
+if ( isset( $_POST['submitpuuha'] ) ) { 
+    
+   /* Tarkistetaan onko luokkaa annettu ja miten se on annettu*/
+   if(!empty($_POST["luokkasailio"])){
+	 $valittuLuokka = $_POST['luokkasailio'];
+   }elseif(!empty($_POST["luokka"])){
+	$luokanNimi=$_POST["luokka"];
+    	$valittuLuokka=Puuhaluokka::AnnaPuuhaLuokanID($luokanNimi);
+   }
+/* Tarkistetaan onko paikkaa annettu ja miten se on annettu*/
+   if(!empty($_POST["paikka"])){
+	 $paikka = $_POST['paikka'];
+   }elseif(!empty($_POST["paikkasailio"])){
+	$paikka=$_POST["paikkasailio"];
+    	
+   }
+   /*Ottaa ajan*/
+ if(!empty($_POST["paiva"]) && !empty($_POST["kellonaika"])){
+	$ajankohta=date_create_from_format("j.n.Y G.i",$_POST['paiva']." ".$_POST['kellonaika']);
+    	$uusiPuuha->setAjankohta($ajankohta); 
+   }elseif(!empty($_POST["paiva"])){
+	$ajankohta=date_create_from_format("j.n.Y G.i",$_POST['paiva']." 00.00"); 
+    	$uusiPuuha->setAjankohta($ajankohta);
+	}
+   
+
+    $uusiPuuha = new Puuhat();
+    $uusiPuuha->setNimi($_POST['nimi']);
+    $uusiPuuha->setKuvaus($_POST['kuvaus']);
+    $uusiPuuha->setHenkilomaara($_POST['henkilomaara']);
+    $uusiPuuha->setKesto($_POST['kesto']);
+    $uusiPuuha->setPaikka($paikka);
+    $uusiPuuha->setPuuhaluokanId($valittuLuokka);
+    $uusiPuuha->setPuuhanLisaysPaiva($date = date('Y-m-d'));
+    $uusiPuuha->setLisaaja(annaKirjautuneenId());
+    $virheet=$uusiPuuha->getVirheet();
+    
+
+    /* Tarksitetaan onko puuha syötetty oikein */
+    if (empty($virheet)) {
+        $uusiPuuha->lisaaKantaan();
+        $_SESSION['ilmoitus'] = "Puuhaluokka lisätty onnistuneesti.";
+        
+        //Luokka lisättiin kantaan onnistuneesti, lähetetään käyttäjä eteenpäin
+        header('Location: puuhatK.php');
+  
+        
+    } else {
+        $virheet = $uusiPuuha->getVirheet();
+
+        //Virheet voidaan nyt välittää näkymälle syötettyjen tietojen kera
+        naytaNakyma("nakymat/puuhanLisays.php", array(
+            'aktiivinen' => "puuhat",
+            'uusiPuuha' => $uusiPuuha,
+	    'luokat' => $luokat,
+	     'taidot' => $taidot,
+            'virhe' => $virheet
+        ));
+    }
+}
+naytaNakyma('nakymat/puuhanLisays.php', array(
+    'aktiivinen' => "puuhat",
+    'uusiPuuha' => new Puuhat(),
+    'luokat' => $luokat,
+ 'taidot' => $taidot
 ));
+
 
