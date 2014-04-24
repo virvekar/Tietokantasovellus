980c91f2741c3bd2531c6f02da7f8071e5c41cf3
Virve E Maanpaa <virvemaa@svm-19.cs.helsinki.fi>
2014-04-24 15:27:34 +0300

    dublikaatti poistettu

--- sql/sql/drop-tables.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE puuhaluokka CASCADE;
-DROP TABLE puuhat CASCADE;
-DROP TABLE henkilo CASCADE;
-DROP TABLE taidot CASCADE;
-DROP TABLE suosikit;
-DROP TABLE suositukset;
-DROP TABLE omatTaidot;
-DROP TABLE puuhaTaidot;
